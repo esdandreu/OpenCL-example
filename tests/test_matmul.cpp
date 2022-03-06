cdb5836330dd9fa806526b76cd1be1fd30077526
esdandreu <esdandreu@gmail.com>
2022-03-06 17:00:29 +0100

    test PrintDevices

--- tests/test_matmul.cpp
+++ tests/test_matmul.cpp
@@ -19,75 +19,28 @@ TEST(OpenCLTest, MatmulFileExists) {
     ASSERT_TRUE(std::filesystem::exists(f));
 }
 
-TEST(OpenCLTest, Debug) {
-    // std::vector<cl::Platform> platforms;
-    // cl::Platform::get(&platforms);
-    // for (auto& p : platforms) {
-    //     std::cout << p.getInfo<CL_PLATFORM_NAME>() << std::endl;
-    //     std::vector<cl::Device> devices;
-    //     p.getDevices(CL_DEVICE_TYPE_ALL, &devices);
-    //     for (auto& d : devices) {
-    //         std::cout << "\t" << d.getInfo<CL_DEVICE_NAME>() << std::endl;
-    //     }
-    // }
+TEST(OpenCLTest, PrintDevices) {
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
-
-    int platform_id = 0;
-    int device_id   = 0;
-
-    std::cout << "Number of Platforms: " << platforms.size() << std::endl;
-
-    for (std::vector<cl::Platform>::iterator it = platforms.begin();
-         it != platforms.end(); ++it) {
-        cl::Platform platform(*it);
-
-        std::cout << "Platform ID: " << platform_id++ << std::endl;
-        std::cout << "Platform Name: " << platform.getInfo<CL_PLATFORM_NAME>()
-                  << std::endl;
-        std::cout
-            << "Platform Vendor: " << platform.getInfo<CL_PLATFORM_VENDOR>()
-            << std::endl;
-
+    for (auto& p : platforms) {
+        std::cout << p.getInfo<CL_PLATFORM_NAME>() << std::endl;
         std::vector<cl::Device> devices;
-        platform.getDevices(CL_DEVICE_TYPE_GPU | CL_DEVICE_TYPE_CPU, &devices);
-
-        for (std::vector<cl::Device>::iterator it2 = devices.begin();
-             it2 != devices.end(); ++it2) {
-            cl::Device device(*it2);
-
-            std::cout << std::endl
-                      << "\tDevice " << device_id++ << ": " << std::endl;
-            std::cout << "\t\tDevice Name: " << device.getInfo<CL_DEVICE_NAME>()
-                      << std::endl;
+        p.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+        for (auto& device : devices) {
             std::cout
-                << "\t\tDevice Vendor: " << device.getInfo<CL_DEVICE_VENDOR>()
+                << "\t" << device.getInfo<CL_DEVICE_NAME>() << std::endl
+                << "\t\tMax Compute Units: "
+                << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << std::endl
+                << "\t\tGlobal Memory: "
+                << device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>() << std::endl
+                << "\t\tMax Clock Frequency: "
+                << device.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>() << std::endl
+                << "\t\tMax Memory Allocation: "
+                << device.getInfo<CL_DEVICE_MAX_MEM_ALLOC_SIZE>() << std::endl
+                << "\t\tLocal Memory: "
+                << device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>() << std::endl
+                << "\t\tAvailable: " << device.getInfo<CL_DEVICE_AVAILABLE>()
                 << std::endl;
-            std::cout
-                << "\t\tDevice Version: " << device.getInfo<CL_DEVICE_VERSION>()
-                << std::endl;
-            switch (device.getInfo<CL_DEVICE_TYPE>()) {
-            case 4: std::cout << "\t\tDevice Type: GPU" << std::endl; break;
-            case 2: std::cout << "\t\tDevice Type: CPU" << std::endl; break;
-            default: std::cout << "\t\tDevice Type: unknown" << std::endl;
-            }
-            std::cout << "\t\tDevice Max Compute Units: "
-                      << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>()
-                      << std::endl;
-            std::cout << "\t\tDevice Global Memory: "
-                      << device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>()
-                      << std::endl;
-            std::cout << "\t\tDevice Max Clock Frequency: "
-                      << device.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>()
-                      << std::endl;
-            std::cout << "\t\tDevice Max Memory Allocation: "
-                      << device.getInfo<CL_DEVICE_MAX_MEM_ALLOC_SIZE>()
-                      << std::endl;
-            std::cout << "\t\tDevice Local Memory: "
-                      << device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>()
-                      << std::endl;
-            std::cout << "\t\tDevice Available: "
-                      << device.getInfo<CL_DEVICE_AVAILABLE>() << std::endl;
         }
         std::cout << std::endl;
     }
